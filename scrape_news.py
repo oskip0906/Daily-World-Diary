a67f46c9169dbd27c4e4bafec5f24b89545f2e11
ricefarmer1 <oskioscar135@gmail.com>
2023-09-08 00:21:09 -0400

    s

--- scrape_news.py
+++ scrape_news.py
@@ -1,11 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+from googlesearch import search
 
-url = 'https://www.bbc.com/news'
-response = requests.get(url)
+query = "world news"
 
-if (response == 200) {
-
-    
-
+search_params = {
+    "query": query,
+    "stop": 10,   
+    "pause": 1,  
 }
+
+search_results = search(**search_params)
+
+news_data = []
+
+for result in search_results:
+
+    try:
+        response = requests.get(result)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        print(f"URL: {result}")
+
+    except Exception as e:
+        print(f"Error scraping {result}: {str(e)}")
