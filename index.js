6b860f3b8d8da676b5e60a225dcc44237204213b
ricefarmer1 <oskioscar135@gmail.com>
2023-09-15 00:46:09 -0400

    Load Diary function

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); 
-const day = currentDate.getDate().toString().padStart(2, '0'); 
-
-const file = `daily_diaries/${year}-${month}-${day}.txt`;
-
 async function fetchFileContent(file) {
-    const response = await fetch(file); 
-    const text = await response.text(); 
+
+    const response = await fetch(file);
+
+    if (!response.ok) {
+        return "The requested diary does not exist.";
+    }
+
+    const text = await response.text();
     return text;
+
 }
 
 
-async function insertFileContent() {
+async function insertFileContent(file, date) {
+
     const diary = document.getElementById('diary');
-    const text = await fetchFileContent(file); 
+    const text = await fetchFileContent(file);
     const textWithLineBreaks = text.replace(/\n/g, '<br>');
-    diary.innerHTML = `<br><h1>${year}-${month}-${day}</h1><br>` + textWithLineBreaks;
+
+    diary.innerHTML = `<br><h1>"${date}"</h1><br>` + textWithLineBreaks;
+
 }
 
-window.addEventListener('load', insertFileContent);
+
+function processDate() {
+
+    var date = document.getElementById("date").value;
+    var file = `daily_diaries/${date}.txt`;
+
+    const loadButton = document.getElementById("loadButton");
+    loadButton.disabled = true;
+
+    insertFileContent(file, date)
+        .then(() => {
+            loadButton.disabled = false;
+        })
+
+}
+
+
+function getCurrentDate() {
+
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dateContainer = document.getElementById("dateContainer");
+    dateContainer.innerHTML = `Current Date: ${getCurrentDate()}`;
+});
